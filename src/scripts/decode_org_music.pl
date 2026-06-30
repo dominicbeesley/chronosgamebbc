3ce7b19f54940d006cebfa8b0d2e16fa6a51ee2a
dominicbeesley <8866586+dominicbeesley@users.noreply.github.com>
2026-06-30 22:08:54 +0100

    got 1st cut pulse-y chords working as a demo

---
+++ src/scripts/decode_org_music.pl
@@ -0,0 +1,103 @@
+#!/usr/bin/env perl
+
+use strict;
+use feature 'try';
+use Fcntl qw(SEEK_SET SEEK_CUR SEEK_END); # better than using 0, 1, 2
+
+$#ARGV == 1 or Usage("Wrong number of arguments");
+
+my ($fn_bin, $fn_asm) = @ARGV;
+
+($fn_bin and -e $fn_bin) or Usage("Cannot find capture file \"$fn_bin\"");
+
+my $STREAM_X_START = 0xEBE4;	# IX stream start
+my $bin = ();
+
+open (my $fh_bin, "<", $fn_bin) or Usage("Cannot open \"$fn_bin\" for input : $!");
+binmode $fh_bin;
+my $l = read($fh_bin, $bin, 0x10000);
+$l == 0x10000 or Usage("$fn_bin is not exactly 64K, expecting a memory dump $l");
+
+close $fh_bin;
+
+# convert $bin to an array of bytes
+my @bin = unpack("C*", $bin);
+
+open (my $fh_asm, ">", $fn_asm) or Usage("Cannot open \"$fn_asm\" for output : $!");
+
+print $fh_asm	"\t\t; Music generated - do not edit\n\n";
+print $fh_asm	"\t\t.include \"music.inc\"\n";
+
+my $offs = $STREAM_X_START;
+my $echo = 0;
+print $fh_asm	"\t\t.export music_x_stream\n";
+printf $fh_asm	"music_x_stream:\t; X stream starts at \$%04X\n", $offs;
+while ($offs < 0x10000) {
+
+	my $x_c = @bin[$offs++];
+
+	if ($x_c == 0x00) {
+		printf $fh_asm "\t\tMX_END\n";
+		printf $fh_asm "\t\t; End of X stream found at \$0x%04X\n\n", $offs;
+		last;
+	} elsif ($x_c == 0x01) {
+		printf $fh_asm "\t\tMX_LOOP_END\n\n"
+	} elsif ($x_c == 0x02) {
+		my $x_r = @bin[$offs++];
+		printf $fh_asm "\n\t\tMX_LOOP_START\t%d\n", $x_r + 1;
+		if (!@bin[$offs]) {
+			printf $fh_asm "\t\tMX_RESET_IY_5C3A\t\t; TODO: check this out?\n";
+			$offs++;
+		}
+	} elsif ($x_c == 0xFF) {
+		my $x_sc = @bin[$offs++];
+		if ($x_sc == 0x01) {
+			my $x_p0 = @bin[$offs++];
+			my $x_p1 = @bin[$offs++];
+			my $x_p2 = @bin[$offs++];
+			my $x_p3 = @bin[$offs++];
+
+			printf $fh_asm "\t\tMX_CMD_PORTA\t%d, %d, %d, %d\n", $x_p0, $x_p1, $x_p2, $x_p3;
+		} elsif ($x_sc == 0x02) {
+			printf $fh_asm "\t\tMX_ARP_RESTART\n";
+		} elsif ($x_sc == 0x03) {
+			printf $fh_asm "\t\tMX_ARP_OFF\n";
+		} elsif ($x_sc == 0x04) {
+			printf $fh_asm "\t\tMX_ARP_ON_1\n";
+		} elsif ($x_sc == 0x05) {
+			printf $fh_asm "\t\tMX_ARP_ON_2\n";
+		} elsif ($x_sc == 0x08) {
+			my $ay_m = @bin[$offs++];
+			printf $fh_asm "\t\tMX_AY_MUL %d\n", $ay_m;
+		} elsif ($x_sc == 0x09) {
+			$echo = 1;
+			print $fh_asm "\t\tMX_ECHO_ON\n";
+		} elsif ($x_sc == 0x0A) {
+			$echo = 0;
+			print $fh_asm "\t\tMX_ECHO_OFF\n";
+		} 
+	} elsif ($echo) {
+		my ($e_p, $h_p, $dur) = ($x_c, @bin[$offs++], @bin[$offs++]);
+		printf $fh_asm "\t\tMX_NOTE_ECHO \$%02X,\$%02X,\$%02X\n", $e_p, $h_p, $dur;
+	} else {
+		my ($e_p, $h_p, $d_p, $dur) = ($x_c, @bin[$offs++], @bin[$offs++], @bin[$offs++]);
+		printf $fh_asm "\t\tMX_NOTE_NECHO \$%02X,\$%02X,\$%02X,\$%02X\n", $e_p, $h_p, $d_p, $dur;		
+	}
+
+}
+
+
+close $fh_asm;
+
+
+
+
+sub Usage($) {
+	my ($msg) = @_;
+
+	print STDERR "ERROR: $msg\n";
+
+	print STDERR "Usage: decode_org_music <z80game.bin> <music_gen.asm>\n";
+
+	exit 10;
+}
--- src/sound.asm
+++ src/sound.asm
@@ -43,6 +43,21 @@
 		.endif
 		.endmacro
 
+		.zeropage
+
+zp_osc_coarseE:	.res	1
+zp_osc_coarseH:	.res	1
+zp_osc_coarseD:	.res	1
+
+zp_beeb256:	.res	1
+zp_song_x_ptr:	.res	2
+
+zp_song_x_dur:	.res	1
+
+zp_song_x_loop_ptr:.res	2
+zp_song_x_loop_ctr:.res	1
+
+
 		.code
 
 		sei
@@ -63,25 +78,196 @@
 ;		POKESN 2, 0, 200
 ;		POKESN 3, 0, 0
 
-		ldx	#0
-@loop:		lda	$C000,X
-		and	#$0F
-		ora	#$90
-		POKEA
-		jsr	wait
-		inx
-		bne	@loop
-		inc	@loop+2
-		bne	@loop
-		lda	#$C0
-		sta	@loop+2
-		jmp	@loop
-			
+;;;; This example plays ROM contents as "white" noise
+;;;;		ldx	#0
+;;;;@loop:		lda	$C000,X
+;;;;		and	#$0F
+;;;;		ora	#$90
+;;;;		POKEA
+;;;;		jsr	wait
+;;;;		inx
+;;;;		bne	@loop
+;;;;		inc	@loop+2
+;;;;		bne	@loop
+;;;;		lda	#$C0
+;;;;		sta	@loop+2
+;;;;		jmp	@loop
+;;;;			
+;;;;
+;;;;wait:		jsr @w2
+;;;;@w2:		jsr @w1
+;;;;@w1:		jsr @w0
+;;;;@w0:		rts
+;;;;
 
-wait:		jsr @w2
-@w2:		jsr @w1
-@w1:		jsr @w0
-@w0:		rts
+
+song_init:
+		ldx	#0
+		stx	zp_beeb256
+		;;stx	_oper_echo_handler+1
+		inx
+		stx	zp_song_x_loop_ctr
+		stx	zp_song_x_dur
+
+		lda	#<music_x_stream
+		sta	zp_song_x_ptr
+		sta	zp_song_x_loop_ptr
+		lda	#>music_x_stream
+		sta	zp_song_x_ptr+1
+		sta	zp_song_x_loop_ptr+1
+
+
+
+
+song_x_parse:	dec	zp_song_x_dur
+		bne	song_x_parse_skip
+
+song_x_parse_againy0:
+		ldy	#0
+		; we assume there's a note before Y runs out!
+song_x_parse_again:
+		lda	(zp_song_x_ptr),Y
+		beq	song_init
+		iny
+		cmp	#1
+		bne	@sk_loop_start
+
+		; start a loop command #1
+		lda	(zp_song_x_ptr),Y
+		iny
+		tax
+		inx
+		stx	zp_song_x_loop_ctr
+		jsr	update_x_ptr
+		ldy	#0
+		lda	zp_song_x_ptr
+		sta	zp_song_x_loop_ptr
+		lda	zp_song_x_ptr+1
+		sta	zp_song_x_loop_ptr+1
+		bne	song_x_parse_again	; always
+
+@sk_loop_start:	cmp	#2
+		bne	@sk_loop_end
+
+		; end a loop command #2
+		dec	zp_song_x_loop_ctr
+		beq	song_x_parse_again	; exit loop, do next note
+		lda	zp_song_x_loop_ptr
+		sta	zp_song_x_ptr		
+		lda	zp_song_x_loop_ptr+1
+		sta	zp_song_x_ptr+1
+		bne	song_x_parse_againy0	; always
+@sk_loop_end:
+
+		sta	oper_coarseE
+		
+		lda	(zp_song_x_ptr),Y
+		iny
+		sta	oper_coarseH
+
+		lda	(zp_song_x_ptr),Y
+		iny
+		sta	oper_coarseD
+
+		lda	(zp_song_x_ptr),Y
+		iny
+		sta	zp_song_x_dur
+
+		jsr	update_x_ptr
+
+song_x_parse_skip:
+
+
+song_beep:
+		jsr	beep_256
+		jsr	beep_256
+		jsr	beep_256
+
+		jmp	song_x_parse
+
+update_x_ptr:
+		tya
+		clc
+		adc	zp_song_x_ptr
+		sta	zp_song_x_ptr
+		lda	zp_song_x_ptr+1
+		adc	#0
+		sta	zp_song_x_ptr+1
+		rts
+
+
+beep_256:
+; Play a tone using variable width pulses with modulation
+beep256_lp:	dec	zp_osc_coarseE
+		bne	skip_osc_H
+
+		; osc D
+		lda	#$90
+oper_coarseE = *-1
+		sta	zp_osc_coarseE
+		lda	#$90
+		POKEA
+		ldx	#10
+@onelp:		dex
+		bne	@onelp
+		lda	#$9F
+		POKEA
+		ldx	#1
+@offelp:		dex
+		bne	@offelp
+
+skip_osc_H:
+
+		dec	zp_osc_coarseH
+		bne	skip_osc_D
+
+		; osc D
+		lda	#$20
+oper_coarseH := *-1
+		sta	zp_osc_coarseH
+		lda	#$90
+		POKEA
+		ldx	#1
+@onhlp:		dex
+		bne	@onhlp
+		lda	#$9F
+		POKEA
+		ldx	#1
+@offhlp:		dex
+		bne	@offhlp
+
+skip_osc_D:
+
+		dec	zp_osc_coarseD
+		bne	skip_osc_done
+
+		; osc E
+		lda	#$16
+oper_coarseD := *-1
+		sta	zp_osc_coarseD
+		lda	#$90
+		POKEA
+		ldx	#4
+@ondlp:		dex
+		bne	@ondlp
+		lda	#$9F
+		POKEA
+		ldx	#1
+@offdlp:		dex
+		bne	@offdlp
+
+skip_osc_done:
+
+		ldx	#5
+@dlp:		jsr	wait
+		dex
+		bne	@dlp
+		dec	zp_beeb256
+		beq	beeb256_ex
+		jmp	beep256_lp
+beeb256_ex:
+wait:		rts
+
 
 
 HERE:		jmp HERE
